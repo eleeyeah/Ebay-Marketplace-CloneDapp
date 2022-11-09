6f4f3381573ec31bc00656a8344dc2480a117e78
eleeyeah <ilietmks@gmail.com>
2022-11-09 08:40:16 +0100

    wallet/nav/search/subcat

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,6 +7,7 @@ import {
   ChevronDownIcon,
   MagnifyingGlassIcon,
 } from "@heroicons/react/24/outline";
+import Image from "next/image";
 
 type Props = {};
 
@@ -43,6 +44,57 @@ function Header({}: Props) {
           <ShoppingCartIcon className="h-6 v-6" />
         </div>
       </nav>
+      <hr className="mt-3" />
+
+      <section className="flex items-center space-x-2 py-5">
+        <div className="h-16 w-16 sm:w-28 md:w-44 cursor-pointer flex-shrink-0">
+          <Link href="/">
+            <Image
+              className="h-full w-full object-contain"
+              alt="logo"
+              src="https://links.papareact.com/bdb"
+              width={100}
+              height={100}
+            />
+          </Link>
+        </div>
+        <button className="hidden lg:flex items-center space-x-2 w-20">
+          <p className="text-gray-600 text-sm">Shop by category</p>
+          <ChevronDownIcon className="h-4 flex-shrink-0" />
+        </button>
+
+        <div className="flex items-center space-x-2 px-2 md:px-5 py-2 border-blac border-2 flex-1">
+          <MagnifyingGlassIcon className="w-5 text-gray-400" />
+          <input
+            className="flex-1 outline-none"
+            placeholder="Search for Anything"
+            type="text"
+          />
+        </div>
+        <button className="hidden sm:inline bg-blue-600 text-white px-5 md:px-10 py-2 border-2 border-blue-600">
+          Search
+        </button>
+        <Link href="/create">
+          <button className="border-2 border-blue-600 px-5 md:px-10 py-2 text-blue-600 hover:bg-blue-600/70 hover:text-white cursor-pointer">
+            List Items
+          </button>
+        </Link>
+      </section>
+      <hr />
+      <section className="flex py-3 space-x-6 text-xs md:text-sm whitespace-nowrap justify-center px-6">
+        <p className="link">Home</p>
+        <p className="link">Electronics</p>
+        <p className="link">Computers</p>
+        <p className="link hidden sm:inline">Video Games</p>
+        <p className="link hidden sm:inline">Home & Garden</p>
+        <p className="link hidden md:inline">Health & Beauty</p>
+        <p className="link hidden lg:inline">Colectibles and Art</p>
+        <p className="link hidden lg:inline">Books</p>
+        <p className="link hidden lg:inline">Music</p>
+        <p className="link hidden xl:inline">Deals</p>
+        <p className="link hidden xl:inline">Other</p>
+        <p className="link ">More</p>
+      </section>
     </div>
   );
 }
