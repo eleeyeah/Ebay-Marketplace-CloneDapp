3f15b1578c66889a18ec49def945395584df2ceb
eleeyeah <ilietmks@gmail.com>
2022-11-12 13:52:12 +0100

    issues with thirdweb listing & rendering listing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,23 @@
 import Header from "../components/Header";
+import {
+  useContract,
+  MediaRenderer,
+  useActiveListings,
+} from "@thirdweb-dev/react";
 
 const Home = () => {
+  const { contract } = useContract(
+    process.env.NEXT_PUBLIC_MARKETPLACE_ADDRESS,
+    "marketplace"
+  );
+
+  const { data: listings, isLoading: loadingListings } =
+    useActiveListings(contract);
+
+  console.log(listings);
+
   return (
     <div>
-      
       <Header />
     </div>
   );
