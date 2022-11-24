93ff5c955026d67bc63120c45b2c3e8504638b8c
eleeyeah <ilietmks@gmail.com>
2022-11-24 21:33:32 +0100

    Listings UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,14 +11,36 @@ const Home = () => {
     "marketplace"
   );
 
-  const { data: listing, isLoading: loadingListings } =
+  const { data: listings, isLoading: loadingListings } =
     useActiveListings(contract);
 
-  console.log(listing);
+  console.log(listings);
 
   return (
     <div>
       <Header />
+      <main className="max-w-6xl mx-auto p-2">
+        {loadingListings ? (
+          <p className="text-center animate-pulse text-blue*-500">Loading...</p>
+        ) : (
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mx-auto">
+            {listings?.map((listing) => (
+              <div
+                className="flex flex-col card hover:scale-105 transition-all duration-150 ease-out"
+                key={listing.id}
+              >
+                <div className="flex-1 flex-col pb-2 items-center">
+                  <MediaRenderer className="w-44" src={listing.asset.image} />
+                </div>
+                <div></div>
+                <div>
+                  <h2>{listing.asset.name}</h2>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </main>
     </div>
   );
 };
